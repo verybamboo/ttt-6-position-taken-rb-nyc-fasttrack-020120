1c7bfd55fc30a3f4cbbe3e63f895d3c86d3aa611
George Chen <georgechen1338@gmail.com>
2020-02-02 17:04:01 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,17 +3,18 @@
 def position_taken?(board, index)
   #((board[index] == "X") || (board[index] == "O"))
   
-  if board[index] = "X"
-    true
-    elsif board[index] == "O"
-      true
-    else
-      false
-  end
+  #if board[index] = "X"
+  #  true
+  #  elsif board[index] == "O"
+  #   true
+  #  else
+  #    false
+  #end
   
   if board[index] == " "
     false
   end
   
   !(board[index] == " ")
+  
 end
